186cc34ae55f087bf3ffce49cefa9fa37ed1f18c
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-17 16:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let ele=document.getElementById("level");
+let res=0;
+let curr=ele;
+while(curr){
+	res++;
+	curr=curr.parentElement;
+}
+alert("The level of the element is: "+res);
+	
